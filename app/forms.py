bfead98e6eb1e07e5c8e54350706db5b6046260f
Francis Ali <francisali692@gmail.com>
2024-01-12 06:11:46 +0100

    Updated forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,12 +5,12 @@ class UpdateForm(forms.Form):
     cv_link = forms.CharField(min_length=1, required=False)
     
     skill_abbr = forms.CharField(label='Skill Abbreviation (Min. of three letters)', max_length=3, min_length=2, required=False)
-    skill_name = forms.CharField(label='Skill name', max_length=15, required=False)
-    description = forms.CharField(max_length=30, 
+    skill_name = forms.CharField(label='Skill name', max_length=25, required=False)
+    description = forms.CharField(max_length=90, 
         widget=forms.Textarea(), required=False)
     
     project_title = forms.CharField(max_length=15, required=False)
     project_display_image = forms.ImageField(required=False)
-    project_link = forms.URLField(max_length=30, required=False)
-    project_description = forms.CharField(max_length=40, 
+    project_link = forms.URLField(max_length=60, required=False)
+    project_description = forms.CharField(max_length=110, 
                                           widget=forms.Textarea(), required=False)
